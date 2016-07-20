02e3eb12a1ad5c1df465289b89ef0e9d61415b40
DeokJin Lee <truevirtue@nexell.co.kr>
2016-07-20 11:50:12 +0900

    bl1: memtester: compile error correction memory test
    This patch was modified to compile a memory test error.
    
    Change-Id: I75f4aabdb03780bee8161fdf6070900f567801a9
    Signed-off-by: DeokJin Lee <truevirtue@nexell.co.kr>

--- src/memtester.c
+++ src/memtester.c
@@ -1,4 +1,4 @@
- *      Copyright (C) 2012 Nexell Co., All Rights Reserved
+/*      Copyright (C) 2012 Nexell Co., All Rights Reserved
  *      Nexell Co. Proprietary & Confidential
  *
  *      NEXELL INFORMS THAT THIS CODE AND INFORMATION IS PROVIDED "AS IS" BASE
