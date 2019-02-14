6875e35583a1650bbdb359c8983992e20a357a09
Jongshin Park <pjsin865@nexell.co.kr>
2019-02-14 16:17:02 +0900

    s5p4418: convergence-daudio: Change the ARM/CORE voltage
    This patch changes the ARM/CORE voltage.
    - ARM  : 1.125V
    - CORE : 1.0V
    
    Signed-off-by: Jongshin Park <pjsin865@nexell.co.kr>
    Change-Id: I9307a54802b48de01c6c410a3c82044b6c3b6b22
    Reviewed-on: https://review.gerrithub.io/c/444517
    Tested-by: Nexell Jenkins <jenkins@nexell.co.kr>
    Reviewed-by: allan park <allan.park@nexell.co.kr>

--- src/board/board_convergence_daudio.c
+++ src/board/board_convergence_daudio.c
@@ -21,22 +21,28 @@
 #include <nxe2000.h>
 #include <mp8845.h>
 
-#define AUTO_VOLTAGE_CONTROL			1
-#define ARM_VOLTAGE_CONTROL_SKIP		0
+#define AUTO_VOLTAGE_CONTROL 		1
+#define ARM_VOLTAGE_CONTROL_SKIP	0
 
-#define NXE2000_I2C_GPIO_GRP 			3	// GROUP : GPIOD
-#define NXE2000_I2C_SCL 				4	// SCL   : GPIOD04
-#define NXE2000_I2C_SDA 				5	// SDA   : GPIOD05
-#define NXE2000_I2C_SCL_ALT 			0	// SCL   : ALT0
-#define NXE2000_I2C_SDA_ALT				0	// SDA   : ALT0
+#define NXE2000_I2C_GPIO_GRP		3 // GROUP : GPIOD
+#define NXE2000_I2C_SCL			4 // SCL   : GPIOD04
+#define NXE2000_I2C_SDA			5 // SDA   : GPIOD05
+#define NXE2000_I2C_SCL_ALT		0 // SCL   : ALT0
+#define NXE2000_I2C_SDA_ALT		0 // SDA   : ALT0
 
-/************************************************
-  * SVT Board (PMIC: NXE2000, MP8845)  - Reference 2016.04.05
-  * ARM		: 1.25V
-  * CORE	: 1.2V --> 1.0
-  * DDR		: 1.5V
-  * DDR_IO	: 1.5V
-  ************************************************/
+/**********************************************
+ * apply Q100 condition *
+ * ARM		: 1.125V
+ * CORE		: 1.0V
+ * DDR		: 1.5V
+ * DDR_IO	: 1.5V
+************************************************/
+#define NXE2000_DEF_DDC1_Q100_VOL	1125000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.3V */
+#define NXE2000_DEF_DDC2_Q100_VOL	1000000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.1V */
+#define NXE2000_DEF_DDC3_Q100_VOL	3300000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 3.3V */
+#define NXE2000_DEF_DDC4_Q100_VOL	1500000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.6V */
+#define NXE2000_DEF_DDC5_Q100_VOL	1500000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.6V */
+
 void pmic_board_init(void)
 {
 	char data[4];
@@ -44,17 +50,16 @@ void pmic_board_init(void)
 	I2C_INIT(NXE2000_I2C_GPIO_GRP, NXE2000_I2C_SCL, NXE2000_I2C_SDA,
 			NXE2000_I2C_SCL_ALT, NXE2000_I2C_SDA_ALT);
 
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC1_VOL);
+	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC1_Q100_VOL);
 	nxe2000_write(NXE2000_REG_DC1VOL, data, 1);
 
-	/* apply Q100 condition CORE(DCDC2) voltage : 1.1V --> 1.0V */
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC2_VOL - 100000);
+	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC2_Q100_VOL);
 	nxe2000_write(NXE2000_REG_DC2VOL, data, 1);
 
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC4_VOL);
+	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC4_Q100_VOL);
 	nxe2000_write(NXE2000_REG_DC4VOL, data, 1);
 
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC5_VOL);
+	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC5_Q100_VOL);
 	nxe2000_write(NXE2000_REG_DC5VOL, data, 1);
 
 	I2C_DEINIT();
