fa396148ef8d387f4a52dcd93b64e7743ba93ab1
Youngbok Park <ybpark@nexell.co.kr>
2019-03-19 10:19:53 +0900

    board: add nxp4330 vtk
    This patch add nxp4330 vtk board.
    
    Signed-off-by: Youngbok Park <ybpark@nexell.co.kr>
    Change-Id: I0ecfa2ac836f76ff25e3cf1fb2ee0a4a65e510b8
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/bl1_s5p4418/+/448341
    Tested-by: Nexell Jenkins <jenkins@nexell.co.kr>
    Reviewed-by: jung hyun kim <jhkim@nexell.co.kr>

---
+++ src/board/board_vtk.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: DeokJin, Lee <truevirtue@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <sysheader.h>
+#if defined(PMIC_ON)
+#include <i2c_gpio.h>
+#include <nxe1500.h>
+
+#define NXE1500_I2C_GPIO_GRP 			3
+#define NXE1500_I2C_SCL 			2
+#define NXE1500_I2C_SDA 			3
+#define NXE1500_I2C_SCL_ALT 			0
+#define NXE1500_I2C_SDA_ALT 			0
+
+/************************************************
+  * Raptor Board (PMIC: NXE2000)  - Reference 2016.04.05
+  * ARM		: 1.25V
+  * CORE	: 1.2V
+  * DDR		: 1.5V
+  * DDR_IO	: 1.5V
+  ************************************************/
+void pmic_board_init(void)
+{
+	char data[4];
+
+	I2C_INIT(NXE1500_I2C_GPIO_GRP, NXE1500_I2C_SCL, NXE1500_I2C_SDA,
+			NXE1500_I2C_SCL_ALT, NXE1500_I2C_SDA_ALT);
+
+	// ARM Voltage (Default: 1.25V)
+	data[0] = nxe1500_get_dcdc_step(NXE1500_DEF_DDC1_VOL);
+	nxe1500_write(NXE1500_REG_DC1VOL, data, 1);
+
+	// Core Voltage (Default: 1.1V)
+	data[0] = nxe1500_get_dcdc_step(NXE1500_DEF_DDC2_VOL);
+	nxe1500_write(NXE1500_REG_DC2VOL, data, 1);
+
+	// DDR I/O Voltage (Default: 1.5V)
+	data[0] = nxe1500_get_dcdc_step(NXE1500_DEF_DDC3_VOL);
+	nxe1500_write(NXE1500_REG_DC3VOL, data, 1);
+}
+#endif
